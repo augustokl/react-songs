b2285c2824d7fd17c1648af8c7291f0bbe9cbb53
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-08-26 22:09:15 -0300

    finish redux sample

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+import SongList from './components/SongList';
+import SongDetail from './components/SongDetail';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Provider store={createStore(reducers)}>
+      <div className="ui container grid">
+        <div className="ui row" style={{marginTop: '30px'}}>
+          <div className="column eight wide">
+            <SongList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
